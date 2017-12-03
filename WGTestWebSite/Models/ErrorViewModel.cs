6c017831cc348b926f20889cb5ce2b4f976591e6
Dain Reinhart <dain@wolfgamesllc.com>
2017-12-03 10:45:22 -0500

    Add project files.

---
+++ WGTestWebSite/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace WGTestWebSite.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
