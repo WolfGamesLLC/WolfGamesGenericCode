e509902c2cc1ad5a43666bb61905a9320f30a0ea
Dain Reinhart <dain@wolfgamesllc.com>
2017-12-03 11:29:10 -0500

    Migrated Controllers

---
+++ WGTestWebSite/Controllers/NamespaceDoc.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WGTestWebSite.Controllers
+{
+    /// <summary>
+    /// The <see cref="WGTestWebSite.Controllers"/> namespace contains controller classes for 
+    /// the application used for testing <see cref="WGSystem"/> classes
+    /// </summary>
+    [System.Runtime.CompilerServices.CompilerGenerated]
+    public class NamespaceDoc
+    {
+    }
+}
---
+++ WGTestWebSite/Controllers/TestListController.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace WGTestWebSite.Controllers
+{
+    /// <summary>
+    /// Controller for testing the generic collections API functions
+    /// </summary>
+    [Produces("application/json")]
+    [Route("api/TestList")]
+    public class TestListController : Controller
+    {
+        /// <summary>
+        /// Contains the generic collections factory to be tested
+        /// </summary>
+        private WGGenericCollectionsFactory wGGenericCollectionsFactory;
+
+        /// <summary>
+        /// Allow the generic collection factory dependency to be injected
+        /// </summary>
+        /// <param name="collectionsFactory">Instance of a WGGenericCollectionsFactory</param>
+        public TestListController(WGGenericCollectionsFactory collectionsFactory)
+        {
+            wGGenericCollectionsFactory = collectionsFactory;
+        }
+
+        /// <summary>
+        /// Implement the HttpGet function of the controller
+        /// </summary>
+        /// <returns>An object that implements IEnumerable</returns>
+        // GET: api/TestList
+        [HttpGet]
+        public IEnumerable<string> Get()
+        {
+            IList<string> data = wGGenericCollectionsFactory.CreateList<string>();
+            data.Add("Key");
+            data.Add("Value");
+
+            return (IEnumerable<string>)data;
+        }
+
+        // GET: api/TestList/5
+        [HttpGet("{id}", Name = "Get")]
+        public string Get(int id)
+        {
+            return "value";
+        }
+
+        // POST: api/TestList
+        [HttpPost]
+        public void Post([FromBody]string value)
+        {
+        }
+
+        // PUT: api/TestList/5
+        [HttpPut("{id}")]
+        public void Put(int id, [FromBody]string value)
+        {
+        }
+
+        // DELETE: api/ApiWithActions/5
+        [HttpDelete("{id}")]
+        public void Delete(int id)
+        {
+        }
+    }
+}
